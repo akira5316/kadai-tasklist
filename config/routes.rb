51fa6b8512a6ed99978ec2252773a2e365661501
akira.fuwa <akirafuwa@gmail.com>
2018-10-25 08:33:14 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   # CRUD
   root to: 'toppages#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:show, :new, :create]
 end
